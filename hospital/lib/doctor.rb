dda8786ff697d660b9fe06a12b72cff67c3443c5
Jessica Joly & Vaughn Dice <student@epicodus.com>
2015-08-18 16:12:58 -0700

    Add equalizer method to Doctor class

--- hospital/lib/doctor.rb
+++ hospital/lib/doctor.rb
@@ -1,10 +1,31 @@
 class Doctor
-  attr_reader(:name, :specialty)
+  attr_reader(:name, :speciality)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
-    @specialty = attributes.fetch(:specialty)
+    @speciality = attributes.fetch(:speciality)
   end
 
+  define_singleton_method(:all) do
+    returned_hashes = DB.exec("SELECT * FROM doctors;")
+    doctors = []
+    returned_hashes.each() do |hash|
+      name = hash.fetch('name')
+      speciality = hash.fetch('speciality')
+      doctors.push(Doctor.new({:name => name, :speciality => speciality}))
+    end
+    doctors
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO doctors (name,speciality) VALUES ('#{@name}','#{@speciality}');")
+  end
+
+  define_method(:==) do |doctor|
+    same_class = self.class().eql?(doctor.class())
+    same_name = self.name().eql?(doctor.name())
+    same_speciality = self.speciality().eql?(doctor.speciality())
+    same_class.&(same_name).&(same_speciality)
+  end
 
 end
--- hospital/spec/doctor_spec.rb
+++ hospital/spec/doctor_spec.rb
@@ -4,15 +4,38 @@ require('spec_helper')
 describe(Doctor) do
   describe('#name') do
     it('returns the name of the doctor') do
-      doc = Doctor.new({:name => 'Dr. Doolittle', :specialty => 'Veterinarian'})
+      doc = Doctor.new({:name => 'Dr. Doolittle', :speciality => 'Veterinarian'})
       expect(doc.name()).to(eq('Dr. Doolittle'))
     end
   end
 
-  describe('#specialty') do
-    it('returns the specialty of the doctor') do
-      doc = Doctor.new({:name => 'Dr. Doolittle', :specialty => 'Veterinarian'})
-      expect(doc.specialty()).to(eq('Veterinarian'))
+  describe('#speciality') do
+    it('returns the speciality of the doctor') do
+      doc = Doctor.new({:name => 'Dr. Doolittle', :speciality => 'Veterinarian'})
+      expect(doc.speciality()).to(eq('Veterinarian'))
     end
+  end
+
+  describe(".all") do
+    it('initially is empty') do
+      expect(Doctor.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it('should save a doctor to the database') do
+      doc = Doctor.new({:name => 'Dr. Doolittle', :speciality => 'Veterinarian'})
+      doc.save()
+      expect(Doctor.all()).to(eq([doc]))
+    end
+  end
+
+  describe("#==") do
+   it ('should recognize two objects as equal if they have the same properties') do
+     doctor1 = Doctor.new({:name => 'Dr. Doolittle', :speciality => 'Veterinarian'})
+     doctor2 = Doctor.new({:name => 'Dr. Doolittle', :speciality => 'Veterinarian'})
+     expect(doctor1.==(doctor2)).to(eq(true))
+     expect(doctor1).to(eq(doctor2))
+   end
   end
 end
